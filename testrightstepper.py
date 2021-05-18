71652e2bf3f9ce01bdf213543c1dce86741324c0
bustenchops <mmmm_donut@msn.com>
2021-05-17 20:35:47 -0400

    Updated errors...hopefully the tests will go smoothly.

--- testrightstepper.py
+++ testrightstepper.py
@@ -17,9 +17,11 @@ GPIO.setup(enablepin, GPIO.OUT, initial=1)
 GPIO.setup(directionpin, GPIO.OUT, initial=0)
 GPIO.setup(steppin, GPIO.OUT, initial=0)
 
-# SCRIPT
-numberofsteps = 400
-print('enable (0) : direction (0)')
+
+#SCRIPT
+numberofsteps = 1200
+
+quest = input('enable (0) : direction (0)')
 GPIO.output(enablepin, 0)
 GPIO.output(directionpin, 0)
 for x in range(numberofsteps):
@@ -31,7 +33,7 @@ for x in range(numberofsteps):
     else:
         print('LIMIT SWITCH TRIGGER')
 
-print('enable (1) : direction (0)')
+quest = input('enable (1) : direction (0)')
 GPIO.output(enablepin, 1)
 GPIO.output(directionpin, 0)
 for x in range(numberofsteps):
@@ -43,7 +45,7 @@ for x in range(numberofsteps):
     else:
         print('LIMIT SWITCH TRIGGER')
 
-print('enable (0) : direction (1)')
+quest = input('enable (0) : direction (1)')
 GPIO.output(enablepin, 0)
 GPIO.output(directionpin, 1)
 for x in range(numberofsteps):
@@ -55,8 +57,8 @@ for x in range(numberofsteps):
     else:
         print('LIMIT SWITCH TRIGGER')
 
-print('enable (1) : direction (1)')
-GPIO.outpupt(enablepin, 1)
+quest = input('enable (1) : direction (1)')
+GPIO.output(enablepin, 1)
 GPIO.output(directionpin, 1)
 for x in range(numberofsteps):
     if GPIO.input(limitpin):
